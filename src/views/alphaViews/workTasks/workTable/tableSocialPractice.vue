c82286ce587c575595f83645d2207432cabc73f8
Hi_Rick <1185696945@qq.com>
2020-12-04 21:08:14 +0800

    新增未改

--- src/views/alphaViews/workTasks/workTable/tableSocialPractice.vue
+++ src/views/alphaViews/workTasks/workTable/tableSocialPractice.vue
@@ -5,21 +5,21 @@
       <div style="float: right;margin-right: 1.5rem"><el-button type="text" size="medium" @click="addMatch">新增</el-button></div>
     </div>
     <el-table :data="matchData" style="width: 100%" stripe>
-      <el-table-column prop="academicname" label="单位名称" >
+      <el-table-column prop="unitName" label="单位名称" >
       </el-table-column>
-      <el-table-column prop="academicform" label="实践内容">
+      <el-table-column prop="practiceContent" label="实践内容">
       </el-table-column>
-      <el-table-column prop="academicform" label="到款额">
+      <el-table-column prop="practiceMoney" label="到款额">
       </el-table-column>
-      <el-table-column prop="academictime" label="实践开始时间" >
+      <el-table-column prop="startDate" label="实践开始时间" >
       </el-table-column>
-      <el-table-column prop="academictime" label="实践结束时间" >
+      <el-table-column prop="endDate" label="实践结束时间" >
       </el-table-column>
       <el-table-column label="审核状态">
         <template slot-scope="scope">
-          <el-tag  v-if="scope.row.academicprogress==='未开始'" type="danger" >审核不通过</el-tag>
-          <el-tag  v-if="scope.row.academicprogress==='待审核'" >审核中</el-tag>
-          <el-tag  v-if="scope.row.academicprogress==='已结束'" type="success">审核通过</el-tag>
+          <el-tag  v-if="scope.row.auditStatus==='未开始'" type="danger" >审核不通过</el-tag>
+          <el-tag  v-if="scope.row.auditStatus==='待审核'" >审核中</el-tag>
+          <el-tag  v-if="scope.row.auditStatus==='已结束'" type="success">审核通过</el-tag>
         </template>
       </el-table-column>
       <!--      <el-table-column  label="详情">-->
@@ -49,28 +49,28 @@
       <el-dialog :visible.sync="addMatchVisible" :title="title">
         <el-form ref="form" :model="form" label-width="100px">
           <el-form-item label="单位名称">
-            <el-input v-model="form.academicname"/>
+            <el-input v-model="form.unitName"/>
           </el-form-item>
           <el-form-item label="实践内容">
-            <el-input v-model="form.academicname"/>
+            <el-input v-model="form.practiceContent"/>
           </el-form-item>
           <el-form-item label="到款额">
-            <el-input v-model="form.academicname"/>
+            <el-input v-model="form.practiceMoney"/>
           </el-form-item>
           <el-form-item label="实践开始时间">
           <el-col :span="11">
-            <el-date-picker v-model="form.academictime" value-format=" yyyy-MM-dd " format="yyyy-MM-dd " type="date" placeholder="选择日期" style="width: 60%;"/>
+            <el-date-picker v-model="form.startDate" value-format=" yyyy-MM-dd " format="yyyy-MM-dd " type="date" placeholder="选择日期" style="width: 60%;"/>
           </el-col>
         </el-form-item>
           <el-form-item label="实践结束时间">
             <el-col :span="11">
-              <el-date-picker v-model="form.academictime" value-format=" yyyy-MM-dd " format="yyyy-MM-dd " type="date" placeholder="选择日期" style="width: 60%;"/>
+              <el-date-picker v-model="form.endDate" value-format=" yyyy-MM-dd " format="yyyy-MM-dd " type="date" placeholder="选择日期" style="width: 60%;"/>
             </el-col>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="addMatchVisible = false">取 消</el-button>
-          <el-button type="primary" @click="submitMatchSuccess">确 定</el-button>
+          <el-button type="primary" @click="submitMatchSuccess('form')">确 定</el-button>
         </div>
       </el-dialog>
     </div>
@@ -78,6 +78,7 @@
 </template>
 
 <script>
+import {getSocialdo,updateSocialdo} from "@/api/allTaskData.js"
     export default {
       name: "tableSocialPractice",
       data(){
@@ -87,8 +88,12 @@
           title:'',
           form:{},
           detail:{},
+           tecUsername: 'rmyzAdmin',
           matchData: []
         }
+      },
+      mounted(){
+    this.getTableData(this.tecUsername);
       },
       methods: {
         handleSizeChange(val) {
@@ -99,15 +104,23 @@
         },
         reset(){
           this.form = {
-            matchname:undefined,
-            matchtime:undefined,
-            matchsub:undefined,
-            matchprogress:undefined,
-            matchwork:undefined,
-            matchresult:undefined,
-            beizhu:undefined,
+            tecUsername:'',
+            unitName:undefined,
+            practiceContent:undefined,
+            practiceMoney:undefined,
+            startDate:undefined,
+            endDate:undefined,
+          
           }
         },
+        //  getlist(){
+        //    getSocialdo({
+        //     tecUsername:'rmyzAdmin',
+        //    }).then( res => {
+        //      console.log('res',res)
+        //      this.matchData = res.data.data
+        //    })
+        // },
         viewDetail(row){
           this.detailFlag = true
           this.title = '详细信息'
@@ -124,12 +137,25 @@
           this.form = row
           this.addMatchVisible = true;
         },
-        submitMatchSuccess(){
-          this.$message({
-            type:'success',
-            message:'提交成功'
-          })
-          this.addMatchVisible = false
+        submitMatchSuccess(formName){
+           this.$refs[formName].validate((valid) => {
+           if (valid) {
+              this.form.tecUsername = this.tecUsername
+              updateSocialdo(this.form).then(response => {
+                if (response.data.msg === '成功') {
+                  this.$message({
+                    type: 'success',
+                    message: '添加成功'
+                  });
+                  this.getTableData(this.tecUsername);
+                  this.addMatchVisible = false;
+                }
+              })
+            } else {
+              console.log('添加失败');
+              return false;
+            }
+      });
         },
         deletework() {
           this.$confirm('确认删除此条信息?', '提示', {
@@ -147,7 +173,14 @@
               message: '已取消删除'
             });
           });
-        }
+        },
+         getTableData(tecUsername) {
+         getSocialdo({
+            tecUsername: tecUsername
+          }).then(response => {
+            this.matchData = response.data.data
+          })
+        },
       }
     }
 </script>
